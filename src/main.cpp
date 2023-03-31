3e4eeef8aca9b55a085a8e08d3de73391ec50b53
Stanislav <wasi4ic@gmail.com>
2023-03-31 22:33:03 +0200

    updated source

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+#define __IMPORT(module, name) __attribute__((__import_module__(#module), __import_name__(#name)))
+
+
+extern "C" void init() __IMPORT(polyfill, init());
+
 int main() {
+    init();
+
     printf("Hello World\n");
+//    rust_function();
+    
+    char buf[6] = "123\0";
+
+    printf("Before call: %s\n", buf);
+//    __ic_custom_random_get(buf, 4);
+    printf("After call: %s\n", buf);
+ 
     return 0;
 }
 
