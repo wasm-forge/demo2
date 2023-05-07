258864a9333c0a9a36f0989e458b46029325b197
Stanislav Golubtsov <sgaflv@gmail.com>
2023-05-07 17:35:50 +0200

    minor typo fixes

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define __EXPORT(name) __attribute__((__export_name__(#name)))
 
 
-extern "C" void init() __IMPORT(ic_wasi_polyfill, init);
+extern "C" void init() __IMPORT(polyfill, init);
 
 extern "C" void ic0_debug_print(const char *str, int len) __IMPORT(ic0, debug_print);
 extern "C" int  ic0_msg_arg_data_size() __IMPORT(ic0, msg_arg_data_size);
