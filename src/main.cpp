2ff1e7a0d37f65537b47aff9749783d16825bff5
Stanislav Golubtsov <sgaflv@gmail.com>
2023-05-07 17:08:10 +0200

    update dependences

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,13 @@
 #define __EXPORT(name) __attribute__((__export_name__(#name)))
 
 
-extern "C" void init() __IMPORT(polyfill, init);
+extern "C" void init() __IMPORT(ic_wasi_polyfill, init);
 
 extern "C" void ic0_debug_print(const char *str, int len) __IMPORT(ic0, debug_print);
 extern "C" int  ic0_msg_arg_data_size() __IMPORT(ic0, msg_arg_data_size);
 extern "C" void ic0_msg_arg_data_copy(char * buf, int offset, int length) __IMPORT(ic0, msg_arg_data_copy);
 extern "C" void ic0_msg_reply() __IMPORT(ic0, msg_reply);
 extern "C" void ic0_msg_reply_data_append(const char * buf, int length) __IMPORT(ic0, msg_reply_data_append);
-
-
 
 extern "C" __EXPORT(canister_query greet) __attribute__((noinline)) void greet()  {
 
@@ -38,4 +36,3 @@ int main() {
     
     return 0;
 }
-
